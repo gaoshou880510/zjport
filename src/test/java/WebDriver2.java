51724bbab5eae181567de91841c954ff5f629fa4
Administrator <332010328@qq.com>
2018-05-08 10:30:46 +0800

    web

---
+++ src/test/java/WebDriver2.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class WebDriver2 {
+
+
+
+    public static void main(String[] args) {
+        WebDriver driver;
+        driver = new InternetExplorerDriver();
+        driver.navigate().to("http://www.baidu.com");
+
+    }
+}
